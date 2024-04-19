17956abe3f13bf99b2e37579ccaeb3a47f50b87a
Jared Seymour <33576838+jjarhead24@users.noreply.github.com>
2024-04-19 17:54:26 +0100

    19/4/24

--- Gnomes_on_the_roam 5.2 5.2/Saved/ShaderDebugInfo/PCD3D_SM6/M_Landscape_4801e1ed16518749/Default/FLandscapeVertexFactory/TBasePassPSFNoLightMapPolicy/0/CompileDXC.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-SET DXC="D:\Unreal Engine\5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
-IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "D:\Unreal Engine\5.2\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
-	GOTO :END
-)
-%DXC% /auto-binding-space 0 /Zpr /O3 -Wno-parentheses-equality -disable-lifetime-markers /T ps_6_6 /E MainPS /Fc BasePassPixelShader.d3dasm /Fo BasePassPixelShader.dxil BasePassPixelShader.usf
-:END
-PAUSE
